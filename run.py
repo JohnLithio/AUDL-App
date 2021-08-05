e2680497fe8e546d39cd37dfd74ebeefc598585d
John <jlithio@gmail.com>
2021-08-04 23:36:30 -0400

    Update data

--- run.py
+++ run.py
@@ -14,8 +14,16 @@ def open_browser():
 #       Player names
 #       Game info
 #       Teams
-# TODO: Put s3 download as 2nd highest priority, ahead of scrape, below local
+# TODO: Make checking S3 optional
 # TODO: Set up script to makes sure all s3 files are up to date and schedule to run weekly
+# TODO: Dan's suggestions:
+#       Have a clear purpose as to why I'm doing this, clear takeaways.
+#           Tool for coaches to analyze teams, etc.
+#       Have a predictive model. The clustering would be good too.
+#           Perhaps a game score type metric (like EDGE)
+#           Predict next pass or possession outcome based on more than just position
+#               # of throws before, distances of throws, time left, players on field, prior game completion %, etc.
+#               Could then perhaps create player metrics
 if __name__ == "__main__":
     # Timer(1, open_browser).start()
     app.run_server(debug=False)
